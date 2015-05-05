f04eb9a397fa0f432cb2d3b6c8a89150b2993332
Python Adrien <adrien.python@hesge.ch>
2015-05-05 21:18:45 +0200

    TwicTab ListView to display translation and collocations

--- app/src/main/java/ch/unige/Twic/Twic/tabs/TwicTab.java
+++ app/src/main/java/ch/unige/Twic/Twic/tabs/TwicTab.java
@@ -2,11 +2,16 @@ package ch.unige.Twic.Twic.tabs;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import ch.unige.Twic.MainActivity;
@@ -16,17 +21,58 @@ import ch.unige.Twic.Twic.TranslationInfo;
 import ch.unige.Twic.Twic.TwicUrlBuilder;
 import ch.unige.Twic.Twic.TwicXmlParser;
 import ch.unige.Twic.WebService;
+import android.widget.SimpleAdapter;
 
 public class TwicTab extends Fragment implements ManagableTab{
 
-    private TextView baseForm;
+    private ListView listTranslations, listCollocationSrc, listCollocationDst, listBaseForm;
+
+    private void setWordList(ListView list, String[] words) {
+        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+
+        for (String word : words) {
+            if (!(word.isEmpty() || word.startsWith("*"))) {
+                HashMap<String, String> map;
+                map = new HashMap<String, String>();
+                map.put("word", word);
+                listItem.add(map);
+            }
+        }
+
+        SimpleAdapter mSchedule = new SimpleAdapter (getActivity().getBaseContext(), listItem,
+                R.layout.word_with_sound_item, new String[] {"word"}, new int[] {R.id.word});
+        list.setAdapter(mSchedule);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.twic, container, false);
-        baseForm = (TextView) v.findViewById(R.id.baseForm);
+
+        AdapterView.OnItemClickListener wordListsOnClickListener =  new AdapterView.OnItemClickListener() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                HashMap<String, String> map = (HashMap<String, String>) ((ListView)a).getItemAtPosition(position);
+
+                // Play sound
+                Log.e("IST_", "word: " + map.get("word"));
+
+            }
+        };
+
+
+        listTranslations = (ListView) v.findViewById(R.id.listTranslations);
+        listCollocationSrc = (ListView) v.findViewById(R.id.listCollocationSrc);
+        listCollocationDst = (ListView) v.findViewById(R.id.listCollocationDst);
+        listBaseForm = (ListView) v.findViewById(R.id.listBaseForm);
+
+        listTranslations.setOnItemClickListener(wordListsOnClickListener);
+        listCollocationSrc.setOnItemClickListener(wordListsOnClickListener);
+        listCollocationDst.setOnItemClickListener(wordListsOnClickListener);
+        listBaseForm.setOnItemClickListener(wordListsOnClickListener);
 
         try {
             MainActivity.cleanFlash();
@@ -44,7 +90,11 @@ public class TwicTab extends Fragment implements ManagableTab{
             String response = null;
             response = WebService.callUrl(path);
             Map<String, String[]> parseData = TwicXmlParser.parseTwicResponse(response);
-            baseForm.setText(parseData.get("baseForm")[0]);
+
+            setWordList(listBaseForm, parseData.get("baseForm"));
+            setWordList(listTranslations, parseData.get("translation"));
+            setWordList(listCollocationSrc, parseData.get("collocationSource"));
+            setWordList(listCollocationDst, parseData.get("collocationTarget"));
         }
     }
 }
